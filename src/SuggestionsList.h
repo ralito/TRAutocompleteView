d2579c827754aec15caff7ba7adfbf13771652d1
Amit Malhotra <amit.malhotra@trackvia.com>
2013-08-27 15:00:59 -0600

    Adding the SuggestionList to correct place

---
+++ src/SuggestionsList.h
@@ -0,0 +1,22 @@
+//
+//  SuggestionMenu.h
+//  AutoComplete
+//
+//  Created by Wojciech Mandrysz on 19/09/2011.
+//  Copyright 2011 http://tetek.me . All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SuggestionsList : UITableViewController 
+
+-(id)initWithArray:(NSArray*)array;
+-(void)showSuggestionsFor:(UITextField*)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString*)string;
+-(void)showSuggestionsFor:(UITextField*)textField;
+
+@property(retain)NSArray *stringsArray;
+@property(retain)NSArray *matchedStrings;
+@property(retain)UIPopoverController *popOver;
+
+@property(assign)UITextField *activeTextField;
+@end
