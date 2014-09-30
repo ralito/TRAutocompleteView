f94847dce5a970dd5bebe2160b801517a2ae51cb
Amit Malhotra <amit.m@me.com>
2014-09-29 21:22:40 -0600

    cell text label wrapping

--- src/SuggestionsList.h
+++ src/SuggestionsList.h
@@ -1,27 +1,46 @@
 //
-//  SuggestionMenu.h
-//  AutoComplete
+// Copyright (c) 2013, Taras Roshko
+// All rights reserved.
 //
-//  Created by Wojciech Mandrysz on 19/09/2011.
-//  Copyright 2011 http://tetek.me . All rights reserved.
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+// ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+// WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+// ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+// (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+// LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+// ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+// (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+//
+// The views and conclusions contained in the software and documentation are those
+// of the authors and should not be interpreted as representing official policies,
+// either expressed or implied, of the FreeBSD Project.
 //
 
-#import <UIKit/UIKit.h>
-#import "TRAutocompleteItemsSource.h"
-#import "FPPopoverController.h"
+#import <Foundation/Foundation.h>
 
-@interface SuggestionsList : UITableViewController 
+@protocol TRSuggestionItem;
 
--(id)initWithAutocompleteItemSource:(id<TRAutocompleteItemsSource>)itemSource andAutocompletionBlock:(didAutocompletionBlock)autocompletionBlock;
--(void)showSuggestionsFor:(UITextField*)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString*)string;
--(void)showSuggestionsFor:(UITextField*)textField;
+@protocol TRAutocompletionCell <NSObject>
 
-@property(retain)NSArray *suggestionsArray;
-@property(retain)NSArray *matchedSuggestions;
-@property(retain) FPPopoverController *popOver;
-@property(retain)id<TRAutocompleteItemsSource> itemSource;
-@property(assign)UITextField *activeTextField;
+- (void)updateWith:(id <TRSuggestionItem>)item;
 
-@property(copy) didAutocompletionBlock autocompletionBlock;
+@end
+
+@protocol TRAutocompletionCellFactory <NSObject>
+
+- (id <TRAutocompletionCell>)createReusableCellWithIdentifier:(NSString *)identifier;
+- (id)initWithCellForegroundColor:(UIColor *)foregroundColor font:(UIFont*)font;
+- (UIFont*)cellFont;
 
 @end
